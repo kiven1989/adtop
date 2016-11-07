eeb991c9a16ce65b2c8cdb21f2a5808647f53f6a
kiven1989 <wangjun3@kaolafm.com>
2016-11-07 10:29:04 +0800

    数据统计更新

--- js/adtop.js
+++ js/adtop.js
@@ -120,66 +120,82 @@
 			
 			//数据
 			option = {
-			    title: {
-			        text: '整体趋势',
-			        left:40
-			    },
-			    tooltip : {
-			        trigger: 'axis'
-			    },
-			    legend: {
-			        data:['激活总量','UV独立访问量','PV访问量']
-			    },
-			    toolbox: {
-			        feature: {
-			            saveAsImage: {}
-			        }
-			    },
-			    grid: {
-			        left: '3%',
-			        right: '4%',
-			        bottom: '3%',
-			        containLabel: true
-			    },
-			    xAxis : [
-			        {
-			            type : 'category',
-			            boundaryGap : false,
-			            data : ['10月20日','10月21日','10月22日','10月23日','10月24日','10月25日','10月26日']
-			        }
-			    ],
-			    yAxis : [
-			        {
-			            type : 'value'
-			        }
-			    ],
-			    series : [
-			        {
-			            name:'激活总量',
-			            type:'line',
-			            stack: '总量',
-			            smooth:true,
-			            areaStyle: {normal: {}},
-			            data:[120, 132, 101, 134, 90, 230, 210]
-			        },
-			        {
-			            name:'UV独立访问量',
-			            type:'line',
-			            stack: '总量',
-			            smooth:true,
-			            areaStyle: {normal: {}},
-			            data:[220, 182, 191, 234, 290, 330, 310]
-			        },
-			        {
-			            name:'PV访问量',
-			            type:'line',
-			            stack: '总量',
-			            smooth:true,
-			            areaStyle: {normal: {}},
-			            data:[150, 232, 201, 154, 190, 330, 410]
-			        }
-			    ]
-			};
+		    title: {
+		        text: '堆叠区域图'
+		    },
+		    tooltip : {
+		        trigger: 'axis'
+		    },
+		    legend: {
+		        data:['邮件营销','联盟广告','视频广告','直接访问','搜索引擎']
+		    },
+		    toolbox: {
+		        feature: {
+		            saveAsImage: {}
+		        }
+		    },
+		    grid: {
+		        left: '3%',
+		        right: '4%',
+		        bottom: '3%',
+		        containLabel: true
+		    },
+		    xAxis : [
+		        {
+		            type : 'category',
+		            boundaryGap : false,
+		            data : ['周一','周二','周三','周四','周五','周六','周日']
+		        }
+		    ],
+		    yAxis : [
+		        {
+		            type : 'value'
+		        }
+		    ],
+		    series : [
+		        {
+		            name:'邮件营销',
+		            type:'line',
+		            stack: '总量',
+		            areaStyle: {normal: {}},
+		            data:[120, 132, 101, 134, 90, 230, 210]
+		        },
+		        {
+		            name:'联盟广告',
+		            type:'line',
+		            stack: '总量',
+		            areaStyle: {normal: {}},
+		            data:[220, 182, 191, 234, 290, 330, 310]
+		        },
+		        {
+		            name:'视频广告',
+		            type:'line',
+		            stack: '总量',
+		            areaStyle: {normal: {}},
+		            data:[150, 232, 201, 154, 190, 330, 410]
+		        },
+		        {
+		            name:'直接访问',
+		            type:'line',
+		            stack: '总量',
+		            areaStyle: {normal: {}},
+		            data:[320, 332, 301, 334, 390, 330, 320]
+		        },
+		        {
+		            name:'搜索引擎',
+		            type:'line',
+		            stack: '总量',
+		            label: {
+		                normal: {
+		                    show: true
+		                }
+		            },
+		            areaStyle: {normal: {}},
+		            data:[820, 932, 901, 934, 1290, 1330, 1320]
+		        }
+		    ]
+		};
+
 			
 			//设置图表实例的配置项以及数据
 			myEchart.setOption(option);
